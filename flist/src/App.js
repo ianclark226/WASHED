f530dab3564139553371d422bddf3e6f110432f8
Ian Clark <ianclark226@gmail.com>
2020-05-30 12:03:19 -0400

    testing

--- flist/src/App.js
+++ flist/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, { useState} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import data from './data.json';
 
-function App() {
+const App = () => {
+  const [people, setPeople] = useState(data);
+  const [likedUsers, setLikedUsers] = useState(data);
+  const [superLikedUsers, setSuperLikedUsers] = useState(data);
+  const [dislikeUsers, setDislikedUsers] = useState(data);
+  const activeUser = 0;
+
+  switch (action) {
+    case 'ADD_TO _LIKED_USER':
+      break;
+      case 'ADD_TO _DISLIKED_USER':
+      break;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+
+      {people[1] ? (
+        <Person
+        key={person[1].id}
+        person={people[1]}
+        modifySuperficialChoices={modifySuperficialChoices}
+        likedUsers={likedUsers}
+        />
+      ): (
+        <Lonely
+        activeUserImage={people[activeUser].image}
+        likedUsers={likedUsers}
+        superLikedUsers={superLikedUsers}
+        />
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ flist/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Logo from './Logo';
+
+const Header = () => {
+    <header>
+        <div className="fl">
+            <button type="button">
+
+            </button>
+        </div>
+
+        <div className="fl">
+            <Logo />
+
+        </div>
+
+        <div className="fl">
+            <button type="button">
+                
+            </button>
+        </div>
+    </header>
+}
